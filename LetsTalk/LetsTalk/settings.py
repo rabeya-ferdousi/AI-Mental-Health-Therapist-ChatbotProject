01ee7fd9669c05e97788fd183ad432ae8abd677b
Nuzhat Zeba <nazhatzeba@gmail.com>
2023-04-25 12:49:53 +0600

    test pull

--- LetsTalk/LetsTalk/settings.py
+++ LetsTalk/LetsTalk/settings.py
@@ -117,10 +117,8 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
-]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "Home/static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
